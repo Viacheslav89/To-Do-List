4a0a142dea571d35ae77c491daa03a67473e086e
To Do List <2113338@mail.ru>
2023-11-18 15:22:03 +0300

    added an event on the button, list styling

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+const input = document.querySelector('.input');
 
+const add = document.querySelector('.add');
+const clear = document.querySelector('.clear');
+
+const list = document.querySelector('.list');
+
+
+
+// добавляю li в list
+add.addEventListener('click', function(event) {
+    const li = document.createElement('li');
+    if (!input.value) return;
+
+    li.innerHTML = `${input.value}<div class="wrapper_svg"><img class="fit-picture perform" src="./svg/perform.svg"><img class="fit-picture delete" src="./svg/delete.svg"><img class="fit-picture edit" src="./svg/edit.svg"></div>`;
+
+    li.classList.add('list_item')
+
+
+
+
+    list.append(li);
+    input.value = '';
+    
+})
+
+
+// удаляю все li с list
+clear.addEventListener('click', function(event) {
+    list.querySelectorAll('li').forEach((li) => li.remove())
+})
 
 

df2968c2591dafa536112fe9d5f8fe905ff6fb61
To Do List <2113338@mail.ru>
2023-11-20 23:25:13 +0300

    changed page layout, styling, JS logic

--- main.js
+++ main.js
@@ -3,44 +3,43 @@ const input = document.querySelector('.input');
 const add = document.querySelector('.add');
 const clear = document.querySelector('.clear');
 
-const list = document.querySelector('.list');
-const list2 = document.querySelector('.list2');
-const list3 = document.querySelector('.list3');
+const todolist = document.querySelector('.todo-list');
 
 
-// добавляю li в list
+todoListArr = [];
+let counter = 1;
+
+
 add.addEventListener('click', function(event) {
-    const li = document.createElement('li');
-    const li2 = document.createElement('li');
-
     if (!input.value) return;
 
-    li.innerHTML = `<div class="wrapper_svg"><img class="fit-picture perform" src="./svg/perform.svg" alt="Выполнить"><img class="fit-picture deleteLi" src="./svg/delete.svg" alt="Удалить"><img class="fit-picture edit" src="./svg/edit.svg" alt="Редактировать"></div>`;
 
-    li2.innerHTML = `<div class="wrapper_svg"><img class="fit-picture perform" src="./svg/perform.svg" alt="Выполнить"><img class="fit-picture deleteLi" src="./svg/delete.svg" alt="Удалить"><img class="fit-picture edit" src="./svg/edit.svg" alt="Редактировать"></div>`;
-
-    const textBox = document.createElement('div');
-    const textBox2 = document.createElement('div');
-
-    textBox.append(input.value);
-    textBox2.append(input.value);
-
-    textBox.classList.add('wrapper_text');
-    textBox2.classList.add('wrapper_text');
-
-    li.classList.add('list_item');
-    li2.classList.add('list_item');
-
-    li.append(textBox);
-    li2.append(textBox2);
-
-
-    list.append(li);
-    list2.append(li2);
+    const todo = {
+        text: input.value,
+        active: false, 
+        id: counter, 
+    }
+    counter++;
 
     input.value = '';
+    todoListArr.push(todo);
+    // todolist.innerHTML = '';
 
-    document.querySelector('#text2').append(li2);
+    const li = document.createElement('li');
+    li.classList.add('todo-list__item');
+
+    const p = document.createElement('p');
+    p.classList.add('todo-list__text');
+    p.append(todo.text);
+
+    const div = document.createElement('div');
+    div.classList.add('todo-list-img__wrapper');
+    div.innerHTML = '<img class="picture perform" src="./svg/perform.svg"><img class="picture del" src="./svg/delete.svg"><img class="picture edit" src="./svg/edit.svg"></img>';
+
+    li.append(p);
+    li.append(div);
+    todolist.append(li);
+    
 })
 
 input.addEventListener('keypress', function(event) {
@@ -50,101 +49,68 @@ input.addEventListener('keypress', function(event) {
 })
 
 
-// переменные SVG
-const perform = document.querySelector('.perform');
-const deleteLi = document.querySelector('.deleteLi');
-const edit = document.querySelector('.edit');
-
-
 // обработчики событий svg
-list.addEventListener('click', function(event) {
-    let li = event.target.closest('li');
+todolist.addEventListener('click', function(event) {
+    const li = event.target.closest('li');
+    const imgWrapper = event.target.closest('.todo-list-img__wrapper');
 
-    // кнопка удалить
-    if (event.target.classList.contains('deleteLi')) {
+    // const ok = event.target.closest('li').querySelector('.ok');
+    // const perform = event.target.closest('li').querySelector('.perform');
+    // const del = event.target.closest('li').querySelector('.del');
+    // const edit = event.target.closest('li').querySelector('.edit');
+
+    // обработчики событий вополнено
+    if (event.target.classList.contains('perform')) {
+        li.classList.add('todo-list__completed');
+        // console.log(todoListArr);
+    }
+
+    // обработчики событий удалить
+    if (event.target.classList.contains('del')) {
         li.remove();
     }
 
-    // кнопка редактировать
+    // обработчики событий редактировать
     if (event.target.classList.contains('edit')) {
-    
-        let wrapperSvg = event.target.closest('li').querySelector('.wrapper_svg');
-        if (wrapperSvg.children.length === 4) return;
-
-        let imgOk = document.createElement('img');
-        imgOk.classList.add('fit-picture', 'ok');
-        imgOk.src = './svg/ok.svg';
-        wrapperSvg.prepend(imgOk);
-
-        let textBox = event.target.closest('li').querySelector('.wrapper_text')
-        console.log(textBox.innerHTML);
+        // li.querySelector('img').classList.remove('hidden');
 
         let inputLi = document.createElement('input');
         inputLi.classList.add('input_li');
 
-        let text = event.target.closest('li').querySelector('.wrapper_text').textContent;
+        let text = li.querySelector('.todo-list__text').textContent;
         inputLi.value = text;
 
-        li.append(inputLi);
-        textBox.innerHTML = '';
 
+        let imgOk = document.createElement('img');
+        imgOk.classList.add('picture', 'ok');
+        imgOk.src = './svg/ok.svg';
+        imgWrapper.prepend(imgOk);
+
+        let textBox = event.target.closest('li').querySelector('.todo-list__text');
+
+        textBox.innerHTML = '';
+        textBox.append(inputLi);
+        inputLi.focus();
+        
+        
         let ok = event.target.closest('li').querySelector('.ok');
         ok.addEventListener('click', function(event) {
+            if (event.target.classList.contains('ok')) {
+                let text = inputLi.value;
+                inputLi.remove();
+                textBox.innerHTML = text;
+                ok.remove();
 
-            let text = inputLi.value;
-            inputLi.remove();
-            textBox.innerHTML = text;
-            ok.remove();
+            }
         })
-    }
 
-    // кнопка выполнено
-    if (event.target.classList.contains('perform')) {
+        
+        inputLi.addEventListener('keypress', function(event) {
+            if (event.key === "Enter") {
+                ok.click();
+            }
+        })
 
-        let textBox = event.target.closest('li');
-
-        // list2.querySelector('li').classList.add('list_active');
-
-        textBox.classList.add('list_item_finish');
-        const completedLi = document.createElement('li');
-        completedLi.classList.add('completed', 'list_item');
-        completedLi.innerHTML = li.innerHTML;
-        document.querySelector('#text3').append(completedLi);
         
     }
 })
-
-
-// удаляю все li с list
-clear.addEventListener('click', function(event) {
-    document.querySelectorAll('li').forEach((li) => li.remove())
-})
-
-
-// радио весь список
-const allRadio = document.querySelector('.allRadio');
-allRadio.addEventListener('click', function(event) {
-    list.classList.remove('hidden');
-    list2.classList.add('hidden');
-    list3.classList.add('hidden');
-})
-
-
-// радио активные
-const activeRadio = document.querySelector('.activeRadio');
-activeRadio.addEventListener('click', function(event) {
-    list2.classList.remove('hidden');
-    list.classList.add('hidden');
-    list3.classList.add('hidden');
-
-})
-
-
-// радио выполненые
-const completedRadio = document.querySelector('.completedRadio');
-completedRadio.addEventListener('click', function(event) {
-    list3.classList.remove('hidden');
-    list.classList.add('hidden');
-    list2.classList.add('hidden');
-})
-

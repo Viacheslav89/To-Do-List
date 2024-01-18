2f382a69dc1f93d2b24de8d6955c870ba9c970ac
To Do List <2113338@mail.ru>
2024-01-18 20:03:00 +0300

    update base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,5 @@ import { defineConfig } from 'vite'
 
 //https://viacheslav89.github.io/To-Do-List/
 export default defineConfig({
-  server: {
-    port: '8080',
-  }
+  base: '/vite-deploy-demo/',
 })
